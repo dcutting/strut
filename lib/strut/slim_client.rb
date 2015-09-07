f1ebccedf436a3875d014e358d905596aa8ff75e
Dan Cutting <dan@cutting.io>
2015-09-07 18:21:17 +0100

    Extract refactor.

--- lib/strut/slim_client.rb
+++ lib/strut/slim_client.rb
@@ -11,19 +11,36 @@ module Strut
     end
 
     def responses_for_commands(commands)
-      serialised_commands = ListSerializer.serialize(commands)
+      socket = prepare_socket
+      encoded_commands = encode_commands(commands)
+      write_commands(socket, encoded_commands)
+      response = read_response(socket)
+      decode_response(response)
+    end
 
-      length = "%06d" % (serialised_commands.length)
-      encoded_commands = "#{length}:#{serialised_commands}"
-
+    def prepare_socket
       socket = TCPSocket.open(@host, @port)
-      version = socket.gets
-      socket.puts(encoded_commands)
+      _version = socket.gets  # Read and ignore the Slim version number from the server.
+      socket
+    end
 
-      length = socket.read(6).to_i    # <length>
-      socket.read(1)                  # :
-      response = socket.read(length)  # <command>
+    def encode_commands(commands)
+      serialised_commands = ListSerializer.serialize(commands)
+      length = ListSerializer.length_string(serialised_commands.length)
+      "#{length}#{serialised_commands}"
+    end
 
+    def write_commands(socket, commands)
+      socket.puts(commands)
+    end
+
+    def read_response(socket)
+      length = socket.read(6).to_i  # <length>
+      socket.read(1)                # :
+      socket.read(length)           # <command>
+    end
+
+    def decode_response(response)
       ListDeserializer.deserialize(response)
     end
   end
