5c3ce0d6df22b9ab41dec29592b952b8f441bc1f
Dan Cutting <dan@cutting.io>
2015-09-07 13:56:44 +0100

    Tidy up.

--- lib/strut.rb
+++ lib/strut.rb
@@ -5,16 +5,12 @@ require "strut/report_formatter"
 
 module Strut
   def self.run(args)
-
     swagger_filename = args.shift
     host = args.shift
     port = args.shift.to_i
 
     yaml = File.read(swagger_filename)
-
-    parser = SwaggerParser.new
-    commands, line_metadata = parser.parse_yaml(yaml)
-
+    commands, line_metadata = SwaggerParser.new.parse_yaml(yaml)
     responses = SlimClient.new(host, port).responses_for_commands(commands)
     report = SlimResponseReportFactory.new.make_report(responses, line_metadata)
     ReportFormatter.new(yaml).format_report(report)
