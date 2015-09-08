331e1e9b7990c828363a523f51a2de308679b67d
Dan Cutting <dan@cutting.io>
2015-09-08 15:56:33 +0100

    Clearer proxy methods.

--- lib/strut/report.rb
+++ lib/strut/report.rb
@@ -10,6 +10,18 @@ module Strut
       @messages = {}
     end
 
+    def add_ok_for_line(line)
+      add_message_for_line(line, REPORT_OK)
+    end
+
+    def add_fail_for_line(line, message)
+      add_message_for_line(line, REPORT_FAIL, message)
+    end
+
+    def add_exception_for_line(line, message)
+      add_message_for_line(line, REPORT_EXCEPTION, message)
+    end
+
     def add_message_for_line(line, type, message = "")
       if @messages[line].nil? or @messages[line].type == :ok
         @messages[line] = ReportMessage.new(type, message)
--- lib/strut/report_builder.rb
+++ lib/strut/report_builder.rb
@@ -10,14 +10,14 @@ module Strut
         command_id = command_id.to_i
         metadata = document.metadata_for_command_id(command_id)
         if result =~ /^__EXCEPTION__:message:(.+)/
-          report.add_message_for_line(metadata.line, REPORT_EXCEPTION, $1)
+          report.add_exception_for_line(metadata.line, $1)
         else
           matched = metadata.expected_value.to_s == result.to_s
           if metadata.expected_value && !matched
             error_message = "Expected #{metadata.expected_value} but got #{result.to_s}"
-            report.add_message_for_line(metadata.line, REPORT_FAIL, error_message)
+            report.add_fail_for_line(metadata.line, error_message)
           else
-            report.add_message_for_line(metadata.line, REPORT_OK)
+            report.add_ok_for_line(metadata.line)
           end
         end
       end
