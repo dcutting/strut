edb26ffaa9756895acfcdb606ab5b25f6110eda1
Dan Cutting <dan@cutting.io>
2015-09-09 17:41:47 +0100

    Extract scenario builder class.

--- lib/strut/parser.rb
+++ lib/strut/parser.rb
@@ -1,6 +1,7 @@
 require "strut/extensions"
 require "strut/document_builder"
 require "strut/interaction_factory"
+require "strut/scenario_builder"
 require "strut/slim_command"
 require "strut/slim_command_factory"
 
@@ -12,6 +13,7 @@ module Strut
       @command_factory = SlimCommandFactory.new
       @document_builder = DocumentBuilder.new
       @interaction_factory = InteractionFactory.new
+      @scenario_builder = ScenarioBuilder.new(@document_builder, @command_factory)
     end
 
     def parse(yaml)
@@ -44,7 +46,7 @@ module Strut
       if node_name.start_with?(X_SCENARIO_PREFIX)
         fixture = node_name.gsub(/^#{X_SCENARIO_PREFIX}/, "")
         interaction = @interaction_factory.make_interaction(path_stack)
-        extract_scenario_for_interaction(interaction, fixture, node)
+        @scenario_builder.extract_scenario_for_interaction(interaction, fixture, node)
       else
         extract_scenarios_for_children(node["value"], path_stack)
       end
@@ -55,73 +57,6 @@ module Strut
       node.each_pair do |child_node_name, child_node|
         next_path_stack = path_stack + [child_node_name]
         extract_scenarios_for_node(child_node_name.to_s, child_node, next_path_stack)
-      end
-    end
-
-    def extract_scenario_for_interaction(interaction, fixture, node)
-      instance = "instance" # TODO
-
-      line = node["line"]
-
-      make_command = make_make_command(line, instance, fixture)
-      @document_builder.append_command(make_command)
-
-      stages = node["value"]
-
-      given_stages = stages_with_names(stages, ["given", "when"])
-      parse_stages(given_stages) { |k, v| make_given_command(k, v, instance) }
-
-      execute_command = make_execute_command(line, instance)
-      @document_builder.append_command(execute_command)
-
-      then_stages = stages_with_names(stages, ["then"])
-      parse_stages(then_stages) { |k, v| make_then_command(k, v, instance) }
-
-      unless interaction.statusCode.nil?
-        status_command = make_status_command(line, instance, interaction.statusCode)
-        @document_builder.append_command(status_command)
-      end
-    end
-
-    def make_given_command(property_name, value_container, instance)
-      line = value_container["line"]
-      value = value_container["value"]
-      metadata = CommandMetadata.new(line)
-      @command_factory.make_call_command(metadata, instance, "set#{property_name}", value)
-    end
-
-    def make_then_command(property_name, value_container, instance)
-      line = value_container["line"]
-      value = value_container["value"]
-      metadata = CommandMetadata.new(line, value)
-      @command_factory.make_call_command(metadata, instance, property_name)
-    end
-
-    def make_execute_command(line, instance)
-      metadata = CommandMetadata.new(line)
-      @command_factory.make_call_command(metadata, instance, "execute")
-    end
-
-    def make_status_command(line, instance, status)
-      metadata = CommandMetadata.new(line, status)
-      @command_factory.make_call_command(metadata, instance, "statusCode")
-    end
-
-    def make_make_command(line, instance, class_name)
-      metadata = CommandMetadata.new(line)
-      @command_factory.make_make_command(metadata, instance, class_name)
-    end
-
-    def stages_with_names(stages, names)
-      names.map { |name| stages[name] }.reject { |stage| stage.nil? }.map { |stage| stage["value"] }
-    end
-
-    def parse_stages(stages, &block)
-      stages.each do |stage|
-        stage.each do |k, v|
-          command = block.call(k, v)
-          @document_builder.append_command(command)
-        end
       end
     end
   end
---
+++ lib/strut/scenario_builder.rb
@@ -0,0 +1,89 @@
+module Strut
+  class ScenarioBuilder
+    def initialize(document_builder, command_factory)
+      @document_builder = document_builder
+      @command_factory = command_factory
+    end
+
+    def extract_scenario_for_interaction(interaction, fixture, node)
+      instance = "instance" # TODO
+      line = node["line"]
+      stages = node["value"]
+
+      append_make_command(line, instance, fixture)
+      append_given_commands(stages, instance)
+      append_execute_command(line, instance)
+      append_then_commands(stages, instance)
+      append_status_command(line, instance, interaction.statusCode)
+    end
+
+    def append_make_command(line, instance, fixture)
+      make_command = make_make_command(line, instance, fixture)
+      @document_builder.append_command(make_command)
+    end
+
+    def append_given_commands(stages, instance)
+      given_stages = stages_with_names(stages, ["given", "when"])
+      parse_stages(given_stages) { |k, v| make_given_command(k, v, instance) }
+    end
+
+    def append_execute_command(line, instance)
+      execute_command = make_execute_command(line, instance)
+      @document_builder.append_command(execute_command)
+    end
+
+    def append_then_commands(stages, instance)
+      then_stages = stages_with_names(stages, ["then"])
+      parse_stages(then_stages) { |k, v| make_then_command(k, v, instance) }
+    end
+
+    def append_status_command(line, instance, statusCode)
+      unless statusCode.nil?
+        status_command = make_status_command(line, instance, statusCode)
+        @document_builder.append_command(status_command)
+      end
+    end
+
+    def make_given_command(property_name, value_container, instance)
+      line = value_container["line"]
+      value = value_container["value"]
+      metadata = CommandMetadata.new(line)
+      @command_factory.make_call_command(metadata, instance, "set#{property_name}", value)
+    end
+
+    def make_then_command(property_name, value_container, instance)
+      line = value_container["line"]
+      value = value_container["value"]
+      metadata = CommandMetadata.new(line, value)
+      @command_factory.make_call_command(metadata, instance, property_name)
+    end
+
+    def make_execute_command(line, instance)
+      metadata = CommandMetadata.new(line)
+      @command_factory.make_call_command(metadata, instance, "execute")
+    end
+
+    def make_status_command(line, instance, status)
+      metadata = CommandMetadata.new(line, status)
+      @command_factory.make_call_command(metadata, instance, "statusCode")
+    end
+
+    def make_make_command(line, instance, class_name)
+      metadata = CommandMetadata.new(line)
+      @command_factory.make_make_command(metadata, instance, class_name)
+    end
+
+    def stages_with_names(stages, names)
+      names.map { |name| stages[name] }.reject { |stage| stage.nil? }.map { |stage| stage["value"] }
+    end
+
+    def parse_stages(stages, &block)
+      stages.each do |stage|
+        stage.each do |k, v|
+          command = block.call(k, v)
+          @document_builder.append_command(command)
+        end
+      end
+    end
+  end
+end
