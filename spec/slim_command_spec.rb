d60b9837b474c18bfb13aac36b7cba3c1711c893
Dan Cutting <dan@cutting.io>
2015-09-22 12:31:58 +0100

    Complete coverage of SlimCommand class.

--- spec/slim_command_spec.rb
+++ spec/slim_command_spec.rb
@@ -3,18 +3,32 @@ require "spec_helper"
 include Strut
 
 describe ImportCommand do
+  before do
+    @sut = ImportCommand.new(5, nil, "my_namespace")
+  end
+
   it 'converts to a rubyslim array' do
-    cmd = ImportCommand.new(5, nil, "my_namespace")
-    cmd_ary = cmd.to_a
+    cmd_ary = @sut.to_a
     expect(cmd_ary).to eq([5, "import", "my_namespace"])
+  end
+
+  it 'prints like this' do
+    expect(@sut.to_s).to eq("[ 5] import my_namespace")
   end
 end
 
 describe MakeCommand do
+  before do
+    @sut = MakeCommand.new(11, nil, "my_instance", "my_class_name")
+  end
+
   it 'converts to a rubyslim array' do
-    cmd = MakeCommand.new(5, nil, "my_instance", "my_class_name")
-    cmd_ary = cmd.to_a
-    expect(cmd_ary).to eq([5, "make", "my_instance", "my_class_name"])
+    cmd_ary = @sut.to_a
+    expect(cmd_ary).to eq([11, "make", "my_instance", "my_class_name"])
+  end
+
+  it 'prints like this' do
+    expect(@sut.to_s).to eq("[11] my_instance = new my_class_name")
   end
 end
 
@@ -30,5 +44,15 @@ describe CallCommand do
     actual = cmd.to_a.last
     expected = 5
     expect(expected).to eq(actual)
+  end
+
+  it 'prints getter like this' do
+    cmd = CallCommand.new(5, nil, "my_instance", "get_value", nil)
+    expect(cmd.to_s).to eq("[ 5] my_instance.get_value()")
+  end
+
+  it 'prints setter like this' do
+    cmd = CallCommand.new(5, nil, "my_instance", "set_value", "hello")
+    expect(cmd.to_s).to eq("[ 5] my_instance.set_value(hello)")
   end
 end
